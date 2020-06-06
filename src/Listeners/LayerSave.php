1afd013a32eb861973bbd251fc98da75578a0bc5
Jason Tempestini <jason@curlymedia.com>
2020-06-06 14:24:02 -0700

    Call cache cleaning on afterSaveCommit

--- src/Listeners/LayerSave.php
+++ src/Listeners/LayerSave.php
@@ -4,9 +4,16 @@
 namespace Stacks\Listeners;
 
 
+use Cake\Cache\Cache;
 use Cake\Event\Event;
 use Cake\Filesystem\Folder;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
+use Cake\Utility\Hash;
 use DebugKit\Model\Table\PanelsTable;
+use Stacks\Constants\CacheCon;
+use Stacks\Model\Table\StacksTable;
+use function collection;
 
 class LayerSave implements \Cake\Event\EventListenerInterface
 {
@@ -17,7 +24,6 @@ class LayerSave implements \Cake\Event\EventListenerInterface
     public function implementedEvents(): array
     {
         $eventMap = [
-//            'Model.afterSave' => 'afterSave',
             'Model.afterSaveCommit' => 'afterSaveCommit',
         ];
         return $eventMap;
@@ -25,22 +31,7 @@ class LayerSave implements \Cake\Event\EventListenerInterface
 
     /**
      * @param $event Event
-     * @param $entity
-     * @param $options
-     */
-    public function afterSave($event, $entity, $options)
-    {
-        if (! in_array($event->getSubject()->getAlias(), ['Panels', 'Requests', 'Preferences'])) {
-//            osd($event);
-            osd($entity);
-            osd($options);
-//            die;
-        }
-    }
-
-    /**
-     * @param $event Event
-     * @param $entity
+     * @param $entity Entity
      * @param $options
      */
     public function afterSaveCommit($event, $entity, $options)
@@ -49,16 +40,41 @@ class LayerSave implements \Cake\Event\EventListenerInterface
             osd('COMMIT!');
 //            osd($event);
             osd($entity);
-            osd($options);
-            $this->getStackTableList();
-//            die;
+            $map = Cache::read(CacheCon::SCKEY, CacheCon::SCCONFIG) ?? $this->compileLayerMap();
+            $table = $entity->getSource();
+            osd($table);
+            osd(Hash::get($map, strtolower($table)));
+            foreach (Hash::get($map, strtolower($table)) as $stackName => $layerNames) {
+                $this->expireStackCaches($stackName, $entity->id, $layerNames);
+            }
+            die;
         }
     }
 
-    public function getStackTableList()
+    protected function compileLayerMap()
     {
         $tableDir = new Folder(APP.'Model'.DS.'Table');
-        osd($tableDir->find('/(.*)StackTable/'));
+        $stackTableList = ($tableDir->find('(.*)StackTable.php'));
+        $classList = collection($stackTableList)
+            ->map(function ($filename){
+                $alias = str_replace('Table.php', '', $filename);
+                TableRegistry::getTableLocator()->get($alias)->compileLayerMapFragment();
+                TableRegistry::getTableLocator()->remove($alias);
+            })->toArray();
+        return Cache::read(CacheCon::SCKEY, CacheCon::SCCONFIG);
+    }
+
+    protected function expireStackCaches($stackName, $id, $layerNames)
+    {
+        /**
+         * @var StacksTable $stackTable
+         */
+        $stackTable = TableRegistry::getTableLocator()->get($stackName);
+        foreach ($layerNames as $layerName) {
+            foreach ($stackTable->distillFromGivenSeed($layerName, [$id]) as $rootId) {
+                $stackTable->deleteCache($rootId);
+            }
+        }
     }
 
 }
